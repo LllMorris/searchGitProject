983561677e178c88a77bb97d9b0f3597bdb8e9df
LllMorris <lucas-morris@hotmail.com>
2024-10-20 21:59:56 -0300

    feito

---
+++ src/components/ItemRepo/style.js
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const ItemContainer = styled.div`
+    width: 80%;
+
+    h3 {
+        font-size: 32px;
+        margin-bottom: 20px;
+        color: #FAFAFA;
+    }
+    
+    p {
+        font-size: 16px;
+        margin-top: 20px;
+        color: #FAFAFA60;
+    }
+
+    a {
+        color: #FF0000;
+    }
+    h4 {
+        color: #FAFAFA60;
+        margin: 20px 0;
+    }
+`
